dc7cb38610c2240f89fb6ff109d60e2cc020c187
Tom Gaillard <LE1826622@crc-lennox.qc.ca>
2023-10-24 00:11:42 -0400

    Need to figures out prisma DB and how to route it via trpc and router

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,6 +3,9 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.db.post.findMany();
+  }),
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
